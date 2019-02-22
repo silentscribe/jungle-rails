79b7f2a587c13dc71597d357738efba3a511cfc5
silentscribe <seelive.i@gmail.com>
2019-02-22 20:46:21 +0000

    model and schema created and validations added to model:

---
+++ db/migrate/20190222203732_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.references :product, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.text :description
+      t.integer :rating
+      t.string :created_at
+      t.string :updated_at
+
+      t.timestamps null: false
+    end
+  end
+end
