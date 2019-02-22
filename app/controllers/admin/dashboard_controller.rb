74416fbd7edb4d6a4f6ca7d9b187166642d97682
silentscribe <seelive.i@gmail.com>
2019-02-22 20:18:27 +0000

    refactored admin auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,13 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  before_filter :http_basic_auth
-
-  def http_basic_auth
-    if ENV['HTTP_AUTH'] =~ %r{(.+)\:(.+)}
-      unless authenticate_with_http_basic { |user, password|  user == $1 && password == $2 }
-        request_http_basic_authentication
-      end
-    end
-  end
+  http_basic_authenticate_with name: "#{ENV['HTTP_AUTH_USER']}", password: "#{ENV['HTTP_AUTH_PASS']}"
   
   def show
   end
