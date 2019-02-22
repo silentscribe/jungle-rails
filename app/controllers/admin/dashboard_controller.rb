b6254ec95055557d493c9aa927117233325d1ff2
silentscribe <seelive.i@gmail.com>
2019-02-22 19:39:08 +0000

    admin authentication added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
+  before_filter :http_basic_auth
+
+  def http_basic_auth
+    if ENV['HTTP_AUTH'] =~ %r{(.+)\:(.+)}
+      unless authenticate_with_http_basic { |user, password|  user == $1 && password == $2 }
+        request_http_basic_authentication
+      end
+    end
+  end
+  
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,13 @@
 class Admin::ProductsController < ApplicationController
+  before_filter :http_basic_auth
+
+  def http_basic_auth
+    if ENV['HTTP_AUTH'] =~ %r{(.+)\:(.+)}
+      unless authenticate_with_http_basic { |user, password|  user == $1 && password == $2 }
+        request_http_basic_authentication
+      end
+    end
+  end
 
   def index
     @products = Product.order(id: :desc).all
