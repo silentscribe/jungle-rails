24f8f1d221875180dcd91d81a6e0727c8a9ef7c1
silentscribe <seelive.i@gmail.com>
2019-02-26 04:57:22 +0000

    authenticate method written of user class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params['user'][:email])
-    # byebug
-    if user && user.authenticate(params['user'][:password])
+    if user = User.authenticate_with_credentials(params['user'][:email], params['user'][:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :password, length: { in: 8..50 }
   
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
+
